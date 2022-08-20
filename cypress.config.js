1f61a961750d26739ca8f9ec7fc1e4494f0fdcbd
Nisa576 <109780759+Nisa576@users.noreply.github.com>
2022-08-20 20:56:38 +0700

    web1

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    // baseUrl: 'https://b899-13-67-75-93.ngrok.io',
+      baseUrl: 'http://localhost:6061/',
+   
+  },
+  reporter: 'mochawesome',
+  reportOptions:{
+    reportDir:'cypress/results',
+    overwrite:false,
+    html: true,
+    json: true
+  }
+});
